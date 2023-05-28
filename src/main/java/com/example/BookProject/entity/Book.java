c15aab1ce97b12bb3030e67b8bc5df540e1dfa93
Liries1231 <liriescsgo@gmail.com>
2023-05-28 19:44:09 +0300

    dasds

---
+++ src/main/java/com/example/BookProject/entity/Book.java
@@ -0,0 +1,41 @@
+package com.example.BookProject.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Integer id;
+    String name;
+
+    String author;
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    Author authorId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Book book = (Book) o;
+        return id != null && Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
+
+
+
